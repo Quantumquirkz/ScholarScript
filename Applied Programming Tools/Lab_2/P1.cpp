210058bd76b0b3c4f68db8bbe41972c18b5c7383
Jhuomar Boskoll Barría Quintero <124087234+Quantumquirk31@users.noreply.github.com>
2024-08-26 20:12:39 -0500

    Update P1.cpp

--- Applied Programming Tools/Lab_2/P1.cpp
+++ Applied Programming Tools/Lab_2/P1.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int sumar_digitos(int numero) {
+    int suma = 0;
+    while (numero > 0) {
+        suma += numero % 10;
+        numero /= 10;
+    }
+    return suma;
+}
+
+int main() {
+    int numero, suma;
+
+    printf("Ingresa un número entero: ");
+    scanf("%d", &numero);
+
+    for (;;) {
+        suma = sumar_digitos(numero);
+        if (suma < 10) {
+            break;
+        }
+        numero = suma;
+    }
+
+    printf("La suma de los dígitos reducida a un solo dígito es: %d\n", suma);
+
+    return 0;
+}
