012f101e20656b0188f6841d9a5f81bb92b4b25a
Jhuomar Boskoll Barría Quintero <124087234+Quantumquirk31@users.noreply.github.com>
2024-08-26 21:50:48 -0500

    Update P3.cpp

--- Applied Programming Tools/Lab_2/P3.cpp
+++ Applied Programming Tools/Lab_2/P3.cpp
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+    int numero_secreto, adivinanza, intentos = 0;
+
+    srand(time(0));
+    numero_secreto = rand() % 100 + 1;  
+
+    printf("¡Adivina el número (entre 1 y 100)!\n");
+
+    while (1) {
+        printf("Ingresa tu adivinanza: ");
+        scanf("%d", &adivinanza);
+
+        for (;;) {
+            intentos++;
+            break;
+        }
+
+        if (adivinanza > numero_secreto) {
+            printf("Demasiado alto. Intenta nuevamente.\n");
+        } else if (adivinanza < numero_secreto) {
+            printf("Demasiado bajo. Intenta nuevamente.\n");
+        } else {
+            printf("¡Felicidades! Adivinaste el número en %d intentos.\n", intentos);
+            break;
+        }
+    }
+
+    return 0;
+}
